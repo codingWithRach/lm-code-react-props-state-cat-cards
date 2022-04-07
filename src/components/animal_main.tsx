d6500220cc09accd24140e52a3970aa02f6846c0
Rachel Miles <rachmiles@gmail.com>
2022-04-07 17:43:10 +0100

    Create AnimalMain component

---
+++ src/components/animal_main.tsx
@@ -0,0 +1,139 @@
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+import AnimalCard from "./animal_card";
+import AnimalRadiobuttons from "./animal_radiobuttons";
+import InputText from "./input_text";
+import InputNumber from "./input_number";
+import Button from "./button";
+
+import { AnimalType } from "../data/animal_type";
+import Animal from "../data/animal";
+import catData from "../data/cat_data";
+import dogData from "../data/dog-data";
+
+interface AnimalMainProps {
+  catCount: number;
+  dogCount: number;
+  setCatCount: Function;
+  setDogCount: Function;
+}
+
+const AnimalMain: React.FC<AnimalMainProps> = ({
+  catCount,
+  setCatCount,
+  dogCount,
+  setDogCount,
+}) => {
+  const currentYear: number = new Date().getFullYear();
+  const [cats, setCats] = useState<Array<Animal>>(catData);
+  const [dogs, setDogs] = useState<Array<Animal>>(dogData);
+  const [selectedAnimalType, setSelectedAnimalType] =
+    useState<AnimalType>("Cat");
+  const [animalName, setAnimalName] = useState<string>("");
+  const [animalSpecies, setAnimalSpecies] = useState<string>("");
+  const [animalFavFoods, setAnimalFavFoods] = useState<string>("");
+  const [animalBirthYear, setAnimalBirthYear] = useState<number>(currentYear);
+
+  //   let catCount = cats.length;
+  //   let dogCount = dogs.length;
+  setCatCount(cats.length);
+  setDogCount(dogs.length);
+
+  const addAnimal = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const animal: Animal = {
+      animalType: selectedAnimalType,
+      name: animalName,
+      species: animalSpecies,
+      favFoods: animalFavFoods.split(", "),
+      birthYear: animalBirthYear,
+      id: uuidv4(),
+    };
+    event.preventDefault();
+    if (selectedAnimalType === "Cat") {
+      setCats([...cats, animal]);
+      catCount = cats.length;
+    } else {
+      setDogs([...dogs, animal]);
+      dogCount = dogs.length;
+    }
+
+    setAnimalName("");
+    setAnimalSpecies("");
+    setAnimalFavFoods("");
+    setAnimalBirthYear(currentYear);
+  };
+
+  const changeAnimal = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    setSelectedAnimalType(event.target.value as AnimalType);
+  };
+
+  return (
+    <>
+      <main>
+        <div className="cards__wrapper">
+          {[...cats, ...dogs].map((animal, index) => (
+            <AnimalCard
+              animalType={animal.animalType}
+              key={animal.id}
+              name={animal.name}
+              species={animal.species}
+              favFoods={animal.favFoods}
+              birthYear={animal.birthYear}
+              animalIndex={index}
+            />
+          ))}
+        </div>
+        <form>
+          <div className={`form form--${selectedAnimalType.toLowerCase()}`}>
+            <h2 className="header__title">Enter details of another animal:</h2>
+            <AnimalRadiobuttons
+              selectedAnimalType={selectedAnimalType}
+              values={["Cat", "Dog"]}
+              onChangeHandler={changeAnimal}
+            />
+            <InputText
+              value={animalName}
+              onChange={setAnimalName}
+              placeholder="Name..."
+              labelText={`Please enter the name of the ${selectedAnimalType.toLowerCase()}`}
+              idText="name"
+            />
+            <br />
+            <InputText
+              value={animalSpecies}
+              onChange={setAnimalSpecies}
+              placeholder="Species..."
+              labelText={`Please enter the ${selectedAnimalType.toLowerCase()} species`}
+              idText="species"
+            />
+            <br />
+            <InputText
+              value={animalFavFoods}
+              onChange={setAnimalFavFoods}
+              placeholder="Favourite foods..."
+              labelText={`Please enter a comma separated list of the ${selectedAnimalType.toLowerCase()}'s favourite foods`}
+              idText="fav_foods"
+            />
+            <br />
+            <InputNumber
+              value={animalBirthYear}
+              onChange={setAnimalBirthYear}
+              min="1990"
+              max={currentYear.toString()}
+              placeholder="Birth year..."
+              labelText={`Please enter the ${selectedAnimalType.toLowerCase()}'s year of birth`}
+              idText="birth_year"
+            />
+            <br />
+            <Button label="Submit" clickFunction={addAnimal} />
+            <br />
+          </div>
+        </form>
+      </main>
+    </>
+  );
+};
+
+export default AnimalMain;
