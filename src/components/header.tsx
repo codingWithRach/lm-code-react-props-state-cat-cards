6ba9b8a0dad68e72653c2e607dcbb8714313cf63
Rachel Miles <rachmiles@gmail.com>
2022-04-08 11:17:13 +0100

    Improve display of headings for small screens

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,7 +6,7 @@ interface HeaderProps {
 const Header: React.FC<HeaderProps> = ({ catCount, dogCount }) => (
   <header className="header__container">
     <h1 className="header__title header__welcome">Welcome to React!</h1>
-    <h2 className="header__title">
+    <h2 className="header__title header__summary">
       There are currently {catCount} Cats and {dogCount} dogs in this App
     </h2>
   </header>
